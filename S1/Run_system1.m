d235485e651fad839a0f5bec21e3371833326f0e
Steve Blandon <Steveblandon@gmail.com>
2016-11-09 12:13:42 -0500

    Revert "optimization with parallel computing"
    This reverts commit 861034dce63e3a2d4309bddbc2c405c77b65bc07.

--- S1/Run_system1.m
+++ S1/Run_system1.m
@@ -1,33 +1,35 @@
 %system controls
 partition = 0;          %1=on, 0=off
 trainGMMs = 1;          %1=on, 0=off
-testGMMs = 0;           %1=on, 0=off
+testGMMs = 1;           %1=on, 0=off
 logData = 1;            %1=on, 0=off
-findBestK = 0;          %1=on, 0=off
+findBestK = 1;          %1=on, 0=off
 epochs = 1;             %number of trials to find best K, max k = k^epochs if k=2
-progressFactor = 0;     %intervals at which progress is shown as a percentage (for feature extraction and testing)
-testRatio = 0.25;       %ratio of data assigned to testset, must repartition data
-k = 2;              % number of mixture components if training GMMs
+showProgress = 'final'; %'final' = partial | 'iter' = on | 'off'
+progressFactor = 0;     %intervals at which progress is shown as a percentage
+testRatio = 0.3;       %ratio of data assigned to testset
+k = 64;              % number of mixture components if training GMMs
 GMMmaxIter = 100000;     % maximum number of iterations to allow for GMM training
 
+
 %dependencies
-addpath('C:\Users\steve\Workshop\Accent Classification Research\S1\Libraries\rastamat');
+path(path,'C:\Users\steve\Workshop\Accent Classification Research\S1\Libraries\rastamat');
 
 %data specification
 if exist('audio','var') == 0
     audio = {};
 %     audio{1}.class = 'brazilian';
-%     audio{1}.path = 'C:\Users\steve\Workshop\FAE corpora\subset_200\BP';
+%     audio{1}.path = 'C:\Users\steve\Workshop\FAE corpora\subset_IT_RU_200\BP';
 %     audio{2}.class = 'mandarin';
-%     audio{2}.path = 'C:\Users\steve\Workshop\FAE corpora\subset_200\MA';
+%     audio{2}.path = 'C:\Users\steve\Workshop\FAE corpora\subset_IT_RU_200\MA';
 %     audio{3}.class = 'russian';
-%     audio{3}.path = 'C:\Users\steve\Workshop\FAE corpora\subset_200\RU';
+%     audio{3}.path = 'C:\Users\steve\Workshop\FAE corpora\subset_IT_RU_200\RU';
 %     audio{4}.class = 'italian';
-%     audio{4}.path = 'C:\Users\steve\Workshop\FAE corpora\subset_200\IT';
+%     audio{4}.path = 'C:\Users\steve\Workshop\FAE corpora\subset_IT_RU_200\IT';
     path = 'C:\Users\steve\Workshop\FAE corpora\fullset';
     classes = dir(path);
     class_count = length(classes);
-    parfor i=3:class_count
+    for i=3:class_count
         audio{i-2}.class =  classes(i).name;
         audio{i-2}.path = [path, '\', classes(i).name];
     end
@@ -37,7 +39,7 @@ end
 
 %get metadata on partitions (trainset/testset)
 dataSize = 0;
-parfor i=1:class_count
+for i=1:class_count
     [audio{i}, sampleCount] = partitionData(audio{i},testRatio, partition);
     dataSize = dataSize + sampleCount;
 end
@@ -48,7 +50,7 @@ partition = 0;
 if findBestK == 0
    epochs = 1;
 end
-detectionRate = zeros(epochs,2);
+errorRate = zeros(epochs,2);
 
 if logData == 1
    diary(['.\Logs\log__',datestr(datetime('now'),'mm-dd-yyyy_HHMMSS'),'.txt']); 
@@ -63,7 +65,7 @@ for epoch=1:epochs
     disp('--------------------------------------------------');
     tic;    %start timer
     if trainGMMs == 1
-        parfor i=1:class_count
+        for i=1:class_count
             if partition == 1 || isfield(audio{i},'trainset') == 0
                 %extract PLP features
                 disp(['extracting PLP features [', audio{i}.class,']...']);
@@ -71,19 +73,17 @@ for epoch=1:epochs
                     audio{i}.path_train, progressFactor);
             end
             %train GMMs
-            disp(['commencing EM optimization [',audio{i}.class,']...']);    
+            disp(['commencing EM optimization [',audio{i}.class,']...']);
             audio{i}.gmmfit = fitgmdist(audio{i}.trainset, k, 'Options',... 
-            statset('maxIter',GMMmaxIter),...
-            'CovarianceType','diagonal','RegularizationValue',1e-12); 
+            statset('Display',showProgress,'maxIter',GMMmaxIter),...
+            'CovarianceType','diagonal','RegularizationValue',0.01); 
+            disp(['EM optimization for GMM complete [', audio{i}.class, '].']);
             conv = 'FALSE';
             if audio{i}.gmmfit.Converged == 1
                 conv = 'TRUE';
             end
-            disp(['EM optimization for GMM complete [', audio{i}.class,...
-                ']>> convergence: ',conv,...
-                '  |  log-nlogl: -',num2str(log(audio{i}.gmmfit.NegativeLogLikelihood)),...
-                '  |  AIC: ',num2str(audio{i}.gmmfit.AIC),...
-                '  |  iterations: ',num2str(audio{i}.gmmfit.NumIterations)]);
+            disp(['converged:',conv,', | AIC:',num2str(audio{i}.gmmfit.AIC)]);
+            disp('...');
         end
 
     end
@@ -93,7 +93,7 @@ for epoch=1:epochs
         disp('creating confusion matrix for testing phase...');
         labels = cell(class_count,1);
         totalSampleCount = 0;
-        counter = 1;
+        counter = 0;
         for i=1:class_count
            labels{i} =  audio{i}.class;
            totalSampleCount = totalSampleCount + length(audio{i}.testset_raw);
@@ -105,19 +105,16 @@ for epoch=1:epochs
         for i=1:class_count
             sampleCount = length(audio{i}.testset_raw);
             for s=1:sampleCount
+                counter = counter + 1;
                 sample = featExtractPLP(audio{i}.testset_raw(s),audio{i}.path_test,progressFactor);
                 for c=1:class_count
-                    likelihood(c) = log(sum(pdf(audio{c}.gmmfit, sample)));     %use pdf 
-%                     [p, likelihood(c)] = posterior(audio{c}.gmmfit, sample);    %use nlogl from posterior
-%                     p = posterior(audio{c}.gmmfit, sample);                     %use log posterior
-%                     likelihood(c) = sum(sum(p));
+                    likelihood(c) = log(sum(pdf(audio{c}.gmmfit, sample)));
                 end
                 [m, indx] = max(likelihood);
                 confMatrix{i+1,indx+1} = confMatrix{i+1,indx+1} + 1;
                 if mod(counter, notifIntervals) == 0
                     disp([num2str(counter/totalSampleCount*100),'% complete...']);
                 end
-                counter = counter + 1;
             end
         end
         %analyze results
@@ -129,19 +126,14 @@ for epoch=1:epochs
     end
     toc;    %check timer
     if findBestK == 1
-       detectionRate(epoch,:) = [k , acc];
+       errorRate(epoch,:) = [k , 1-acc];
     end
-    if k == 1
-        k = 2;
-    else
-        k = k * 2;
-    end
+    k = k * 2;
 end
 
 if findBestK == 1
-   [bestAcc, index] = max(detectionRate(:,2));
-   bestK = detectionRate(index,1);
-   disp(['highest detection rate of ',num2str(bestAcc),' achieved with  k=',num2str(bestK)]);
-   plot(detectionRate(:,1),detectionRate(:,2));
+   [bestErr, index] = min(errorRate(:,2));
+   bestK = errorRate(index,1);
+   disp(['lowest error rate of ',num2str(bestErr),' achieved with  k=',num2str(bestK)]);
 end
 diary off;
